98b308639fe70d9151669fcbd0e772dacb6cdea2
PhanTopik <phantopik@gmail.com>
2025-12-12 11:51:23 +0700

    perbaiki database

--- config.js
+++ config.js
@@ -1,29 +1,46 @@
-const path = require('path');
-const fs = require('fs');
-const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const projectRoot = process.cwd();
-const envPath = path.join(projectRoot, '.env');
-if (fs.existsSync(envPath)) {
-  dotenv.config({ path: envPath });
+let sequelize;
+
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
-  dotenv.config();
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres',
+      port: process.env.DB_PORT || 5432,
+      logging: false,
+    }
+  );
 }
 
-const config = {
-  port: Number(process.env.PORT) || 3000,
-  jwtSecret: process.env.JWT_SECRET || 'dev-secret-change-me',
-  tokenExpiresIn: process.env.JWT_EXPIRES_IN || '1h',
-  clientOrigin: process.env.CLIENT_URL,
-  dataDir: path.join(projectRoot, 'data'),
-  usersFile: path.join(projectRoot, 'data', 'users.json'),
-  dbHost: process.env.DB_HOST || 'localhost',
-  dbPort: Number(process.env.DB_PORT) || 5432,
-  dbName: process.env.DB_NAME || 'unily-database',
-  dbUser: process.env.DB_USER || 'postgres',
-  dbPassword: process.env.DB_PASSWORD || '2454',
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ PostgreSQL Database Connected...');
+    
+    await sequelize.sync({ alter: true }); 
+    console.log('✅ Models Synced to PostgreSQL...');
+  } catch (err) {
+    console.error('❌ Database Connection Error:', err.message);
+    process.exit(1);
+  }
 };
 
-module.exports = config;
-
-
+module.exports = { sequelize, connectDB };
