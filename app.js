b1e54f5ee37dc29e424c45f75ad5937df1f73777
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-12-13 16:32:12 +0700

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: '*',
   })
 );
 
